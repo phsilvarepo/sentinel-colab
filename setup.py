7484d5b148527f4d892ad8ab114c7f2f00247848
phsilvarepo <pedrosilva7320@gmail.com>
2025-11-04 17:02:37 +0000

    Update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fasttrainer",                # Package name
+    name="sentinel-colab",                # Package name
     version="0.1.0",                   # Version
-    author="Your Name",                # Replace with your name
-    author_email="you@example.com",    # Replace with your email
+    author="Pedro Silva",                # Replace with your name
+    author_email="pedrosilva7320@gmail.com",    # Replace with your email
     description="A Colab-friendly package to speed up training of RF-DETR and YOLOv11 models",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/fasttrainer",  # Replace with your repo URL
+    url="https://github.com/phsilvarepo/sentinel-colab",  # Replace with your repo URL
     packages=find_packages(),          # Automatically find all packages
     python_requires=">=3.8",
     install_requires=[
