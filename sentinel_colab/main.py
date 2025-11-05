9c5304250dcc767ff456e8b1edf8cb1ca759ad6e
phsilvarepo <pedrosilva7320@gmail.com>
2025-11-05 10:06:48 +0000

    Correct

--- sentinel_colab/main.py
+++ sentinel_colab/main.py
@@ -17,6 +17,7 @@ class Trainer:
         self.model = self._load_model()
 
     def _install(self, package):
+        print("Entrou")
         subprocess.run([sys.executable, "-m", "pip", "install", *package.split(), "-q"], check=True)
 
     def _load_model(self):
